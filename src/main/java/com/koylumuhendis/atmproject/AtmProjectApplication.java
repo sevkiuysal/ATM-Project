8f0c6b3f24bc3182ab709093d0cf02b61f555a90
sevki uysal <uysalsevki.9@gmail.com>
2024-01-23 00:13:42 +0300

    exclude = {SecurityAutoConfiguration.class} with application.yaml

--- src/main/java/com/koylumuhendis/atmproject/AtmProjectApplication.java
+++ src/main/java/com/koylumuhendis/atmproject/AtmProjectApplication.java
@@ -2,12 +2,24 @@ package com.koylumuhendis.atmproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class} )
+
 public class AtmProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmProjectApplication.class, args);
 	}
+	
+//	 @Bean
+//	    public Docket api() { 
+//	        return new Docket(DocumentationType.SWAGGER_2)  
+//	          .select()                                  
+//	          .apis(RequestHandlerSelectors.any())              
+//	          .paths(PathSelectors.any())                          
+//	          .build();                                           
+//	    }
+	
 
 }
