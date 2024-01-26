aa23bf792a8ae5f9a20e8ea3e675641b15ca5eb6
sevki uysal <uysalsevki.9@gmail.com>
2024-01-26 23:51:31 +0300

    Login Request class created

---
+++ src/main/java/com/koylumuhendis/atmproject/request/LoginRequest.java
@@ -0,0 +1,36 @@
+package com.koylumuhendis.atmproject.request;
+
+public class LoginRequest {
+	
+	private String name;
+	
+	private String password;
+
+	public LoginRequest() {
+	}
+
+	public LoginRequest(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
