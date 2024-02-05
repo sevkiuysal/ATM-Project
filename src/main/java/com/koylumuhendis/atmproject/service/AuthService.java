327af17fee3402bf1e6347eebfa2d42e0b0ee791
sevki uysal <uysalsevki.9@gmail.com>
2024-02-05 23:36:57 +0300

    loginUser method has been created for user and admin on AuthService

--- src/main/java/com/koylumuhendis/atmproject/service/AuthService.java
+++ src/main/java/com/koylumuhendis/atmproject/service/AuthService.java
@@ -1,7 +1,13 @@
 package com.koylumuhendis.atmproject.service;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.koylumuhendis.atmproject.dto.LoginRequest;
+import com.koylumuhendis.atmproject.dto.TokenResponseDto;
+import com.koylumuhendis.atmproject.model.Role;
 import com.koylumuhendis.atmproject.utils.TokenGenerator;
 
 public class AuthService {
@@ -20,6 +26,28 @@ public class AuthService {
 		this.adminService = adminService;
 		this.userService = userService;
 		this.generator = generator;
+	}
+	
+	public TokenResponseDto loginUser(LoginRequest request) {
+		try {
+		
+			Authentication authentication=authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+			TokenResponseDto responseDto=null;
+			if(request.getRole()==Role.USER) {
+				responseDto=new TokenResponseDto(
+					generator.generateToken(authentication), userService.getUserByName(request.getUsername())
+					);
+			}else if(request.getRole()==Role.ADMIN) {
+				responseDto=new TokenResponseDto(
+						generator.generateToken(authentication), adminService.getAdminByName(request.getUsername())
+						);
+			}
+			
+			return responseDto;
+		}catch (Exception e) {
+			throw new UsernameNotFoundException("User not found given request.");
+		}
 	}
 	
 
