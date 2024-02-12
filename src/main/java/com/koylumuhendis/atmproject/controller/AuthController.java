00494c7ceec0dd7f248cdbd19a67db7e89221069
sevki uysal <uysalsevki.9@gmail.com>
2024-02-12 23:56:27 +0300

    Authentication Controller created

---
+++ src/main/java/com/koylumuhendis/atmproject/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.koylumuhendis.atmproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.koylumuhendis.atmproject.dto.LoginRequest;
+import com.koylumuhendis.atmproject.dto.TokenResponseDto;
+import com.koylumuhendis.atmproject.service.AuthService;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+	
+	private AuthService authService;
+
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
+	
+	//http://localhost:8080/api/v1/auth/login
+	@PostMapping("/login")
+	public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequest loginRequest){
+		return ResponseEntity.ok().body(authService.login(loginRequest));
+	}
+
+}
