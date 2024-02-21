a029079cd4656e107ac3043c9df25b2c0e91c0e6
sevki uysal <uysalsevki.9@gmail.com>
2024-02-21 23:49:21 +0300

    Login Request deleted from dto layer(package) and created in the request layer

--- src/main/java/com/koylumuhendis/atmproject/request/LoginRequest.java
+++ src/main/java/com/koylumuhendis/atmproject/request/LoginRequest.java
@@ -1,35 +1,61 @@
 package com.koylumuhendis.atmproject.request;
 
+
+import com.koylumuhendis.atmproject.model.Role;
+
 public class LoginRequest {
 	
-	private String name;
+private String username;
 	
 	private String password;
+	
+	private Role role;
 
 	public LoginRequest() {
 	}
 
-	public LoginRequest(String name, String password) {
-		this.name = name;
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+	public String getUsername() {
+		return username;
 	}
 
 	public String getPassword() {
 		return password;
 	}
-
-	public void setPassword(String password) {
-		this.password = password;
+	
+	public Enum getRole() {
+		return role;
 	}
 	
+	public static class builder{
+		private String username;
+		
+		private String password;
+		
+		private Role role;
+		
+		public builder username(String username) {
+			this.username=username;
+			return this;
+		}
+		public builder password(String password) {
+			this.password=password;
+			return this;
+		}
+		
+		public builder role(Role role) {
+			this.role=role;
+			return this;
+		}
+		public LoginRequest build() {
+			return new LoginRequest(this);
+		}
+	}
+	
+	public LoginRequest(builder builder) {
+		this.username=builder.username;
+		this.password=builder.password;
+		this.role=builder.role;
+	}
 	
 	
 
