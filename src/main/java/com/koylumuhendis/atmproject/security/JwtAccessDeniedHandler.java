cf360029ca3bec318f1fb07c088cfe14202ba159
sevki uysal <uysalsevki.9@gmail.com>
2024-03-02 23:32:41 +0300

    Jwt Access Denied Handler created

---
+++ src/main/java/com/koylumuhendis/atmproject/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package com.koylumuhendis.atmproject.security;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		response.setContentType("application/json");
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		response.getOutputStream().println("\"error\":\""+accessDeniedException.getMessage()+"\"");
+		
+	}
+
+}
